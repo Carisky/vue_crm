3ccf5ba9c123e251e27abe5633a95d1b1eb9b72d
Yehor <egorkorsun7@gmail.com>
2026-08-21 07:29:47 +0200

    bigger logo

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -9,9 +9,12 @@ const buttonLink = computed(() => route.name === 'sign-in' ? '/sign-up' : "/sign
     <main class="bg-neutral-100 min-h-screen">
         <div class="max-w-screen-2xl mx-auto p-4">
             <nav class="flex items-center justify-between">
-                <NuxtLink href="/sign-in" class="block h-[56px] w-[152px] overflow-hidden">
-                    <img src="/TSL%20Silesia%20Collab.svg" width="152" height="56" alt="TSL Silesia Collab"
-                        class="size-full object-cover object-center" />
+                <NuxtLink href="/sign-in" class="block h-[80px] w-[240px] overflow-hidden">
+                    <picture>
+                        <source :srcset="'/TSL%20Silesia%20Collab.svg'" type="image/svg+xml" />
+                        <img src="/TSL%20Silesia%20Collab.png" width="240" height="80" alt="TSL Silesia Collab"
+                            class="size-full object-cover object-center" />
+                    </picture>
                 </NuxtLink>
                 <Button variant="secondary" :as-child="true">
                     <NuxtLink :href="buttonLink">
